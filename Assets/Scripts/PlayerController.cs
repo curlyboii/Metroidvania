4441c85e038c530dc1987ae1e90e1bf3db2b71b5
Vitalii <vetalsofyk@gmail.com>
2023-06-12 23:30:06 +0300

    Flipping The Character

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -25,10 +25,25 @@ public class PlayerController : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
+        //move sideways
         theRB.velocity = new Vector2(Input.GetAxisRaw("Horizontal") * moveSpeed, theRB.velocity.y);
 
+
+        //handle direction change 
+        if(theRB.velocity.x < 0)
+        {
+            transform.localScale = new Vector3(-1f,1f,1f);
+        } else if(theRB.velocity.x > 0)
+        {
+            transform.localScale = Vector3.one;
+        }
+
+        //checking if on the ground
         isOnGround = Physics2D.OverlapCircle(groundPoint.position, .2f, whatIsGround);
 
+
+        //jumping
         if(Input.GetButtonDown("Jump") && isOnGround)
         {
             theRB.velocity = new Vector2(theRB.velocity.x, jumpForce);
