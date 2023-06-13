78031df2aa2164c7e29fb15d336126ad2bb9cf03
Vitalii <vetalsofyk@gmail.com>
2023-06-13 23:52:04 +0300

    Clamping The Camera

--- Assets/Scripts/CameraController.cs
+++ Assets/Scripts/CameraController.cs
@@ -5,11 +5,16 @@ using UnityEngine;
 public class CameraController : MonoBehaviour
 {
     private PlayerController player;
+    public BoxCollider2D boundsBox;
+    private float halfHeight, halfWidth;
 
     // Start is called before the first frame update
     void Start()
     {
           player = FindObjectOfType<PlayerController>();
+
+        halfHeight = Camera.main.orthographicSize;
+        halfWidth = halfHeight * Camera.main.aspect;
     }
 
     // Update is called once per frame
@@ -17,7 +22,10 @@ public class CameraController : MonoBehaviour
     {
         if (player != null)
         {
-            transform.position = new Vector3 (player.transform.position.x, player.transform.position.y, transform.position.z);
+            transform.position = new Vector3 (
+                Mathf.Clamp (player.transform.position.x, boundsBox.bounds.min.x + halfWidth, boundsBox.bounds.max.x - halfWidth),
+                 Mathf.Clamp(player.transform.position.y, boundsBox.bounds.min.y + halfHeight, boundsBox.bounds.max.y - halfHeight),
+                transform.position.z) ;
         }
     }
 }
