1019a954ba9278752deb757a7249f437d9ae327c
Vitalii <vetalsofyk@gmail.com>
2023-06-28 00:38:06 +0300

    Pickup text

--- Assets/Scripts/AbilityUnlock.cs
+++ Assets/Scripts/AbilityUnlock.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class AbilityUnlock : MonoBehaviour
 {
@@ -8,6 +9,9 @@ public class AbilityUnlock : MonoBehaviour
     public bool unlockDoubleJump, unlockDash, unlockBecomeBall, unlockDropBomb;
 
     public GameObject pickupEffect;
+
+    public string unlockMessage;
+    public TMP_Text unlockText;
 
 
     private void OnTriggerEnter2D(Collider2D other)
@@ -43,6 +47,15 @@ public class AbilityUnlock : MonoBehaviour
             }
 
             Instantiate(pickupEffect, transform.position, transform.rotation);
+
+            unlockText.transform.parent.SetParent(null);
+            unlockText.transform.parent.position = transform.position;
+
+            unlockText.text = unlockMessage;
+            unlockText.gameObject.SetActive(true);
+
+            Destroy(unlockText.transform.parent.gameObject, 5f);
+
             Destroy(gameObject);
             
         }
