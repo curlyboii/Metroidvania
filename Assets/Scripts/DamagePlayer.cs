7236ed78ab0a867d04a0d97dbd4039da5ccc85e9
Vitalii <vetalsofyk@gmail.com>
2023-07-04 15:57:46 +0300

    Making The Enemy Explode

--- Assets/Scripts/DamagePlayer.cs
+++ Assets/Scripts/DamagePlayer.cs
@@ -6,6 +6,12 @@ public class DamagePlayer : MonoBehaviour
 {
     public int damageAmount = 1;
 
+    public bool destroyOnDamage;
+    public GameObject destroyEffect;
+
+
+
+
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.tag == "Player")
@@ -27,5 +33,14 @@ public class DamagePlayer : MonoBehaviour
 
         PlayerHealthController.instance.DamagePlayer(damageAmount);
 
+        if(destroyOnDamage)
+        {
+            if(destroyEffect != null)
+            {
+                Instantiate(destroyEffect, transform.position, transform.rotation);
+            }
+
+            Destroy(gameObject);
+        }
     }
 }
